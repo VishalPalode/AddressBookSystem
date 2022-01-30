b01892cadc1f53e054257090d40dd3cd5eb2a354
vishal <vishalpalode9899@gmail.com>
2022-01-30 20:46:36 +0530

    Ability to delete person

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -12,11 +12,12 @@ namespace AddressBook
         //Main  Method / program Entry Point
         static void Main(string[] args)
         {
-            //Showing Welcome Message On console.
-            Console.WriteLine("Welcome to Address Book Progam \n");
-            AddressBook createAddressBook = new AddressBook();
-            createAddressBook.ReadInput();
-            Console.ReadLine();
+                //Showing Welcome Message On console.
+                Console.WriteLine("Welcome to Address Book Progam \n");
+                AddressBook createAddressBook = new AddressBook();
+                createAddressBook.ReadInput();
+                Console.ReadLine();
+            
         }
 
     }
