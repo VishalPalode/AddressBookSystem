d9ed9c99ec09eec3d6d2af73851ab4b6606ae65b
vishal <vishalpalode9899@gmail.com>
2022-01-30 19:02:20 +0530

    Contact Creation

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -8,11 +8,14 @@ namespace AddressBook
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("welcome to address book system");
-            Console.ReadLine();
+       
+             public string FirstName { get; set; }
+             public string LastName { get; set; }
+             public string PhoneNumber { get; set; }
+             public string[] Addresses { get; set; }
+             public int zip { get; set; }
+             public string email { get; set; }
+             public static List<Program> People = new List<Program>();
         }
-
     }
-}
+
