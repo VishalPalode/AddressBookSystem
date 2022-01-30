735c7e9d15a98d08d529589d42087f83c9bd836c
vishal <vishalpalode9899@gmail.com>
2022-01-30 20:10:46 +0530

    Add new contact

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -15,20 +15,10 @@ namespace AddressBook
             //Showing Welcome Message On console.
             Console.WriteLine("Welcome to Address Book Progam \n");
 
-            Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
-            AddressBook addressBook = new AddressBook()  //Initializing elements using collection-initializer syntax
-            {
-                FirstName = Console.ReadLine(),
-                LastName = Console.ReadLine(),
-                Address = Console.ReadLine(),
-                City = Console.ReadLine(),
-                State = Console.ReadLine(),
-                Zip = Convert.ToDouble(Console.ReadLine()),
-                PhoneNumber = Convert.ToDouble(Console.ReadLine()),
-                Email = Console.ReadLine(),
-            };
-            addressBook.displayContact();
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddContact();
             Console.ReadLine();
         }
+
     }
 }
